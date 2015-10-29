3d998b266c32fffe7f92b91a365f0c3be4207eaf
ljuarezr <ljuarezr@uaberta.ca>
2015-10-28 19:55:06 -0600

    Added FriendAdapter. More methods on friend test.

--- app/src/androidTest/java/com/gracehoppers/jlovas/bookwrm/FriendsScreenTest.java
+++ app/src/androidTest/java/com/gracehoppers/jlovas/bookwrm/FriendsScreenTest.java
@@ -36,10 +36,64 @@ public class FriendsScreenTest extends ActivityInstrumentationTestCase2  {
         assertTrue(accountA.getFriends().hasFriend(accountB));
         assertTrue(accountB.getFriends().hasFriend(accountA));
 
-
-
-
     }
 
+    public void testUnFriend() throws IllegalEmailException, NoSpacesException, TooLongException{
+        //Create 2 accounts first
+        Account accountA = new Account();
+        Account accountB = new Account();
+
+        accountA.setUsername("userA");
+        accountB.setUsername("userB");
+        accountA.setCity("YEG");
+        accountB.setCity("YEG");
+        accountA.setEmail("a@gmail.com");
+        accountB.setEmail("b@gmail.com");
+
+        //account befriends account
+        accountA.getFriends().addFriend(accountB);
+        accountB.getFriends().addFriend(accountA);
+
+        //assert that A has B as friend and vice versa
+        assertTrue(accountA.getFriends().hasFriend(accountB));
+        assertTrue(accountB.getFriends().hasFriend(accountA));
+
+        //account unfriends account
+        accountA.getFriends().unFriend(accountB);
+        accountB.getFriends().unFriend(accountA);
+
+        //assert that A has B as friend and vice versa
+        assertFalse(accountA.getFriends().hasFriend(accountB));
+        assertFalse(accountB.getFriends().hasFriend(accountA));
+    }
+
+    public void testGetFriend() throws IllegalEmailException, NoSpacesException, TooLongException{
+        //Create 2 accounts first
+        Account accountA = new Account();
+        Account accountB = new Account();
+
+        accountA.setUsername("userA");
+        accountB.setUsername("userB");
+        accountA.setCity("YEG");
+        accountB.setCity("YEG");
+        accountA.setEmail("a@gmail.com");
+        accountB.setEmail("b@gmail.com");
+
+        //account befriends account
+        accountA.getFriends().addFriend(accountB);
+        accountB.getFriends().addFriend(accountA);
+
+        //assert that A has B as friend and vice versa
+        assertTrue(accountA.getFriends().hasFriend(accountB));
+        assertTrue(accountB.getFriends().hasFriend(accountA));
+
+        //from AccountA, get AccountB
+        String friendUsername = accountB.getUsername();
+        Account returnedFriend = accountA.getFriends().getFriend(friendUsername);
+
+        assertTrue(accountB.getUsername() == returnedFriend.getUsername());
+        assertTrue(accountB.getCity() == returnedFriend.getCity());
+        assertTrue(accountB.getEmail() == returnedFriend.getEmail());
+    }
 
 }
