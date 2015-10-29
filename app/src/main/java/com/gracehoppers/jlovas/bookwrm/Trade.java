15b5ebc7de41a2859043bbd2c8ae0477c5d3700a
CH16 <chen1@cs.ualberta.ca>
2015-10-29 16:45:51 -0600

    trying to start CreateTrade UI and trade class

--- app/src/main/java/com/gracehoppers/jlovas/bookwrm/Trade.java
+++ app/src/main/java/com/gracehoppers/jlovas/bookwrm/Trade.java
@@ -1,5 +1,10 @@
 package com.gracehoppers.jlovas.bookwrm;
 
+import android.app.Activity;
+import android.net.wifi.WifiConfiguration;
+
+import java.util.Stack;
+
 /**
  * trade holds the information about the trade:
  * -who it is between
@@ -9,9 +14,32 @@ package com.gracehoppers.jlovas.bookwrm;
 public class Trade {
     private TradeStatus status;
     //is there a need for trade and createTrade to be separate?
+    private int tradeID;
+    private Account borrower;
+    private Account owner;
+    private Book borrowerItem;
+    private Book ownerItem;
 
-    public Trade(){
 
+    public Trade(Account borrower, Account owner, TradeStatus status){
+        this.borrower = borrower;
+        this.owner = owner;
+        this.status = status;
+
+    }
+
+
+    public void createTrade(Book borrowerItemFromInventory, Book ownerItemFromInventory){
+        this.borrowerItem = borrowerItemFromInventory;
+        this.ownerItem = ownerItemFromInventory;
+    }
+
+    public void changeStatus(TradeStatus newStatus){
+        this.status = newStatus;
+    }
+
+    public TradeStatus getStatus(){
+        return status;
     }
 
 }
