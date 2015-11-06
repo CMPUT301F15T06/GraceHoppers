946e13d97a3cceb122849d9b2c5490b80eba2241
nlovas <nlovas@ualberta.ca>
2015-11-05 22:28:21 -0700

    viewbook test isnt working anymore, trying to fix it :C

---
+++ app/src/androidTest/java/com/gracehoppers/jlovas/bookwrm/EditBookActivityTest.java
@@ -0,0 +1,33 @@
+package com.gracehoppers.jlovas.bookwrm;
+
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by nlovas on 11/5/15.
+ */
+public class EditBookActivityTest extends ActivityInstrumentationTestCase2 {
+    /*
+    UI tests for the edit book screen
+     */
+
+
+    public EditBookActivityTest(Class activityClass) {
+        super(com.gracehoppers.jlovas.bookwrm.EditBookActivity.class);
+    }
+
+    public void testStart() throws Exception {
+        Activity activity = getActivity();
+    }
+
+    public void testEdit(){
+        //test to see that the
+
+
+
+    }
+
+
+
+
+}
