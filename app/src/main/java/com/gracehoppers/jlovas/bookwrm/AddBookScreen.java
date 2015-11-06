708a6be3662355c39acebc09c2e1653ccf5ebd37
shadownater <shadownater@hotmail.com>
2015-11-06 13:23:32 -0700

    Javadocs

--- app/src/main/java/com/gracehoppers/jlovas/bookwrm/AddBookScreen.java
+++ app/src/main/java/com/gracehoppers/jlovas/bookwrm/AddBookScreen.java
@@ -196,7 +196,7 @@ public class AddBookScreen extends ActionBarActivity {
             }
         });
 
-        
+
         okButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
