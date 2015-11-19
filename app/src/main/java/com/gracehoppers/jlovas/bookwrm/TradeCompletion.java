959e4520e1af216010ce3aa1aab33878f97be74f
ljuarezr <ljuarezr@uaberta.ca>
2015-11-18 19:46:43 -0700

    Added TradeCompletion, modified TradeStatus. Trying to test TradeHistory, but need to Create a trade first.

---
+++ app/src/main/java/com/gracehoppers/jlovas/bookwrm/TradeCompletion.java
@@ -0,0 +1,19 @@
+package com.gracehoppers.jlovas.bookwrm;
+
+/**
+ * Created by ljuarezr on 11/18/15.
+ */
+/**
+ * This class is an enum for the trade completion (current/complete)
+ *
+ * @Author ljuarezr
+ *
+ * @see Trade
+ */
+public enum TradeCompletion {
+        /**
+         * Enumeration where each item describes the completion of a trade.
+         */
+        CURRENT, COMPLETE;
+}
+
